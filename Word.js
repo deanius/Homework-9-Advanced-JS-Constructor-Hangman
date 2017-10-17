4b7c3d86c752c5063cb059de66ed7180fb07f411
unknown <aliarfeen911@gmail.com>
2017-10-17 14:44:21 -0500

    first commit

---
+++ Word.js
@@ -0,0 +1,2 @@
+var possibleWords =  ["Imran", "Eva", "Jean-Christophe", "Nicole", "Ali", "Grant", "Wolverine", "Batman", "Black Panther", "Superman", "Iron Man", "Aquaman", "Wonder Woman", "Hawkeye", "Spider-Man", "Daredevil", "Green Arrow", "Black Canary", "Captain America", "Batgirl", "Hellboy", "Hulk", "Iron Fist", "Catwoman", "Elektra", "Ghost Rider", "Thor", "Robin", "Silver Surfer", "The Human Torch", "Black Widow", "Professor Xavier", "Beast", "Black Lightning", "Captain Marvel", "Doctor Strange", "The Vision", "Ant-Man", "Supergirl", "Captain Planet"];
+
