63053af8739f230f5dd63b27c00f756ebd6293c9
unknown <aliarfeen911@gmail.com>
2017-10-19 10:15:46 -0500

    minor change in format

--- CLI.js
+++ CLI.js
@@ -26,56 +26,57 @@ function newGame () {
 
 	inquirer.prompt(readyPrompt).then(response => {
 		// create new game object using the user's response
-		var game = new Game(response.category);
+		var game = new Game(response.category.toLowerCase());
 		// display 
 		console.log(`\n  ${game.displayWord}\n`);
 		
-		function guessLoop () {
-
-			function guessALetter() {
-				var guessALetterPrompt = {
-					type: "input",
-					message: "Guess a letter!".input,
-					name: "guessedLetter"
-				};
-
-				return inquirer.prompt(guessALetterPrompt)
-			};
-
-			var guessAllLetters = Promise.resolve();
-
-			if (game.gameOver === false) {
-
-				guessAllLetters = guessAllLetters
-				.then(guessALetter)
-				.then(response => game.evaluateLetter(response.guessedLetter.toUpperCase()))
-				.then(() => console.log(`\nIncorrect Guesses: ${game.displayIncorrectGuesses}`))
-				.then(() => console.log(`Lives Remaining: ${game.livesRemaining}`))
-				.then(() => console.log(`\n  ${game.displayWord}\n`))
-				.then(() => game.evaluateGameState())
-				.then(() => guessLoop())
-			} else {
-				var playAgainPrompt = {
-					type: "list",
-					message: "Play again?",
-					choices: ["Yes!", "No"],
-					name: "ready"
-				};
-
-				inquirer.prompt(playAgainPrompt).then(response => {
-					if(response.ready === "Yes!") {
-						newGame();
-					} else {
-						return;
-					};
-				});
-			};
-		}; // guessLoop(){}
-
-		guessLoop();
+		guessLoop(game);
 
 	}); // inquirer.gameTypePrompt()
 }; // newGame(){}
+
+function guessLoop(game) {
+	// var game = game;
+
+	function guessALetter() {
+		var guessALetterPrompt = {
+			type: "input",
+			message: "Guess a letter!".input,
+			name: "guessedLetter"
+		};
+
+		return inquirer.prompt(guessALetterPrompt)
+	};
+
+	var guessAllLetters = Promise.resolve();
+
+	if (game.gameOver === false) {
+
+		guessAllLetters = guessAllLetters
+		.then(guessALetter)
+		.then(response => game.evaluateLetter(response.guessedLetter.toUpperCase()))
+		.then(() => console.log(`\nIncorrect Guesses: ${game.displayIncorrectGuesses}`))
+		.then(() => console.log(`Lives Remaining: ${game.livesRemaining}`))
+		.then(() => console.log(`\n  ${game.displayWord}\n`))
+		.then(() => game.evaluateGameState())
+		.then(() => guessLoop(game))
+	} else {
+		var playAgainPrompt = {
+			type: "list",
+			message: "Play again?",
+			choices: ["Yes!", "No"],
+			name: "ready"
+		};
+
+		inquirer.prompt(playAgainPrompt).then(response => {
+			if(response.ready === "Yes!") {
+				newGame();
+			} else {
+				return;
+			};
+		});
+	};
+}; // guessLoop(){}
 
 console.log("Welcome to Hangman!".silly.bold);
 newGame();
--- game.js
+++ game.js
@@ -1,9 +1,8 @@
 var Word = require("./Word.js");
 
-function Superhero (category) {
-	// console.log("Superhero(){}");
-	this.Superheroes = ["Wolverine", "Batman", "Black Panther", "Superman", "Iron Man", "Aquaman", "Wonder Woman", "Hawkeye", "Spider-Man", "Daredevil", "Green Arrow", "Black Canary", "Captain America", "Batgirl", "Hellboy", "Hulk", "Iron Fist", "Catwoman", "Elektra", "Ghost Rider", "Thor", "Robin", "Silver Surfer", "The Human Torch", "Black Widow", "Professor Xavier", "Beast", "Black Lightning", "Captain Marvel", "Doctor Strange", "The Vision", "Ant-Man", "Supergirl", "Captain Planet"];
-	this.Classmates = ["Imran", "Eva", "Jean-Christophe", "Nicole", "Ali", "Grant", "Andrew", "Taiwo", "Brett", "Stephanie", "Esdras", "Dean", "Scott", "Ola", "Josie", "Kaitlyn", "Casey", "Jason", "Dartaniel", "Julia", "Edge", "Yamini", "Alina", "Ammad", "Humera", "Julian", "Max", "Michael", "Nicholas"];
+function Game (category) {
+	this.superheroes = ["Wolverine", "Batman", "Black Panther", "Superman", "Iron Man", "Aquaman", "Wonder Woman", "Hawkeye", "Spider-Man", "Daredevil", "Green Arrow", "Black Canary", "Captain America", "Batgirl", "Hellboy", "Hulk", "Iron Fist", "Catwoman", "Elektra", "Ghost Rider", "Thor", "Robin", "Silver Surfer", "The Human Torch", "Black Widow", "Professor Xavier", "Beast", "Black Lightning", "Captain Marvel", "Doctor Strange", "The Vision", "Ant-Man", "Supergirl", "Captain Planet"];
+	this.classmates = ["Imran", "Eva", "Jean-Christophe", "Nicole", "Ali", "Grant", "Andrew", "Taiwo", "Brett", "Stephanie", "Esdras", "Dean", "Scott", "Ola", "Josie", "Kaitlyn", "Casey", "Jason", "Dartaniel", "Julia", "Edge", "Yamini", "Alina", "Ammad", "Humera", "Julian", "Max", "Michael", "Nicholas"];
 
 	this.targetWord = this[category][Math.floor(Math.random() * this[category].length)];
 	
@@ -17,7 +16,7 @@ function Superhero (category) {
 	this.gameOver = false;
 };
 
-Superhero.prototype.evaluateLetter = function(guessedLetter) {
+Game.prototype.evaluateLetter = function(guessedLetter) {
 	// for validating that key presses are actually letters
 	var alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 	
@@ -48,7 +47,7 @@ Superhero.prototype.evaluateLetter = function(guessedLetter) {
 	};
 }; // evaluateLetter(){}
 
-Superhero.prototype.evaluateGameState = function () {
+Game.prototype.evaluateGameState = function () {
 	if(this.displayWord.indexOf("_") === -1) {
 		console.log("\nYou win!\n".green);
 		this.gameOver = true;
@@ -58,4 +57,4 @@ Superhero.prototype.evaluateGameState = function () {
 	};
 }; // evaluateGameState(){}
 
-module.exports = Superhero;
+module.exports = Game;
